053c043a1020b3d71370a23c21bccf127c2f057c
lxyxynt <lxyxynt@gmail.com>
2013-08-27 23:46:57 +0800

    Update Edit Distance.cpp

--- code/Edit Distance.cpp
+++ code/Edit Distance.cpp
@@ -3,7 +3,9 @@ public:
     int minDistance(string word1, string word2) {
         static int f[1111][1111];
         int n = word1.size(), m = word2.size();
-        memset(f,0x3f,sizeof(f));
+        for (int i=1;i<=n;++i)
+            for (int j=1;j<=m;++j)
+                f[i][j] = n+m;
         for (int i=0;i<=n;++i) f[i][0]=i;
         for (int i=0;i<=m;++i) f[0][i]=i;
         for (int i=1;i<=n;++i)
